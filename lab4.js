1b0b804cb874d4cbdf7853feb33cd0c382f846a3
Isaac Alphonso <ialpho26@umd.edu>
2025-02-23 14:25:24 -0500

    feat: added the JS portion

---
+++ lab4.js
@@ -0,0 +1,38 @@
+const bod = document.body;
+bod.style.background = "aliceblue"
+const counter = 0;
+
+document.getElementById("color_changer").addEventListener("click", function() {
+
+    if (bod.style.background === "aliceblue"){
+        bod.style.background = "lightpink"   
+    } else {
+        bod.style.background = "aliceblue"
+    }
+});
+
+document.getElementById("alert_but").addEventListener("click", function() {
+    var usrName = document.getElementById("usr_name").value;
+    
+    if (usrName.trim() == "") {
+        alert("ENTER A NAME!!!!!")
+        return;
+    }
+
+    alert("Hi, " + usrName + "!");
+});
+
+document.getElementById("val_test").addEventListener("click", function() {
+    var validation = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+    var usrText = document.getElementById("tester_text").value;
+    
+    if (validation.test(usrText)) {
+        alert("YOU HAVE A SPECIAL CHARACTER");
+    }
+});
+
+document.getElementById("add_text_but").addEventListener("click", function() {
+    var header = document.getElementById("head");
+    
+    header.textContent += " add text ";
+});
